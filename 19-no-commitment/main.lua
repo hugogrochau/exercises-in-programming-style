fe757057c5ee2862b7299266dca740479a9f336c
Marcelo Politzer Couto <mpolitzer.c@gmail.com>
2016-05-10 07:50:09 -0300

    PRE+POS+ARG

--- 19-no-commitment/main.lua
+++ 19-no-commitment/main.lua
@@ -1,9 +1,16 @@
+-- PRE: config, words, frequencys devem existir.
+--      words e frequencys devem estar devidamente pre-compiladas
+-- POS: twords e tffreqs devem ter carregado as funções esperadas por esse programa.
+-- ARG: é esperado que isso seja seguido por ser a proposta do estilo do código.
+-- @note: nomes foram seguidos por serem os originais e alterá-los poderia ser confuso.
+--        em outra situação os nomes seriam alterados para outros mais legiveis.
 function load_plugins()
 	local config  = require("config")
 	tfwords = require(config.words)
 	tffreqs = require(config.frequencys)
 end
 
+-- main
 load_plugins()
 ordered_words = tffreqs.top25(tfwords.extract_words(arg[1]))
 
